3f87168365c8c0543147364a97a44e274d918c70
Kévin <kevin.jose@abtasty.com>
2020-12-07 15:46:24 +0100

    .

--- http/deploy.sh
+++ http/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 GOOS=linux GOARCH=amd64 go build -o go-service
-scp -i ~/.ssh/kp-20201204.pem go-service ubuntu@ec2-35-180-47-155.eu-west-3.compute.amazonaws.com:
-scp -i ~/.ssh/kp-20201204.pem -r templates ubuntu@ec2-35-180-47-155.eu-west-3.compute.amazonaws.com:
-scp -i ~/.ssh/kp-20201204.pem -r assets ubuntu@ec2-35-180-47-155.eu-west-3.compute.amazonaws.com:
+scp -i ~/.ssh/kp-20201204.pem go-service ubuntu@ec2-35-180-47-155.eu-west-3.compute.amazonaws.com:go-service
+scp -i ~/.ssh/kp-20201204.pem -r templates ubuntu@ec2-35-180-47-155.eu-west-3.compute.amazonaws.com:go-service
+scp -i ~/.ssh/kp-20201204.pem -r assets ubuntu@ec2-35-180-47-155.eu-west-3.compute.amazonaws.com:go-service
 ssh -i ~/.ssh/kp-20201204.pem ubuntu@ec2-35-180-47-155.eu-west-3.compute.amazonaws.com "sudo chmod 700 go-service;
     sudo systemctl stop go-sandbox.service;
     sudo touch /etc/systemd/system/go-sandbox.service;
@@ -10,8 +10,8 @@ ssh -i ~/.ssh/kp-20201204.pem ubuntu@ec2-35-180-47-155.eu-west-3.compute.amazona
 Description=Go server
 
 [Service]
-ExecStart=/home/ubuntu/go-service
-WorkingDirectory=/home/ubuntu
+ExecStart=/home/ubuntu/go-service/go-service
+WorkingDirectory=/home/ubuntu/go-service
 User=root
 Group=root
 Restart=always
